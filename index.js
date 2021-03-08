3eb2bc57b8fb15fc2c1b0e4854166a11f15c8abd
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-03-08 21:36:36 +0000

    exercise starwars

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const axios = require('axios');
 
 http.createServer((req, res)=>{
   //res.write("Hello world \n"); // write a response
-  res.write(users.join("\n"))
+  //res.write(users.join("\n"))
+  res.write(chars.join("\n"))
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
 
-let users = []; // names of users will be stored here
+//let users = []; // names of users will be stored here
+let chars = [];
 (async function getNames() {
   try {
-    const { data } = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    users = data.map(user => user.name);
+    //const { data } = await axios.get(
+    //  "https://jsonplaceholder.typicode.com/users"
+    //);
+    //users = data.map(user => user.name);
+    const { data } = await axios.get("https://swapi.dev/api/people");
+    chars = data.results.map(char => char.name)
+    console.log(chars);
   } catch (error) {
     console.log(error);
   }
