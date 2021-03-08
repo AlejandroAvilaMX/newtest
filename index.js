89526212d5cf5e9886f11835746b9cffc2f98a1d
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-03-08 21:11:11 +0000

    wk 3 code

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const http = require('http');
+const axios = require('axios');
 
 http.createServer((req, res)=>{
-  res.write("Hello world \n"); // write a response
+  //res.write("Hello world \n"); // write a response
+  res.write(users.join("\n"))
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
+
+let users = []; // names of users will be stored here
+(async function getNames() {
+  try {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    users = data.map(user => user.name);
+  } catch (error) {
+    console.log(error);
+  }
+})();
