193fdadf42f94f52c7e84e3c140c5975796863c7
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-03-31 23:10:59 +0000

    run mongodb

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const http = require('http'), 
+const http = require('http'),
 axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
 
 var app = express();
 var port = 8000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(logger('tiny'));
-app.use(require('./route'));
+app.use(require('./routes'));
 
 //app.get('/hello/:foo/:bar', (req, res) => {
 //    //res.send('Hello World!');
@@ -32,21 +33,41 @@ app.use(require('./route'));
 //}).listen(8000); // listen for requests on port 8000
 
 //let users = []; // names of users will be stored here
-let chars = [];
-(async function getNames() {
-  try {
-    //const { data } = await axios.get(
-    //  "https://jsonplaceholder.typicode.com/users"
-    //);
-    //users = data.map(user => user.name);
-    const { data } = await axios.get("https://swapi.dev/api/people");
-    chars = data.results.map(char => char.name)
-    console.log(chars);
-  } catch (error) {
-    console.log(error);
-  }
-})();
+//let chars = [];
+//(async function getNames() {
+//  try {
+//    //const { data } = await axios.get(
+//    //  "https://jsonplaceholder.typicode.com/users"
+//    //);
+//    //users = data.map(user => user.name);
+//    const { data } = await axios.get("https://swapi.dev/api/people");
+//    chars = data.results.map(char => char.name)
+//    console.log(chars);
+//  } catch (error) {
+//    console.log(error);
+//  }
+//})();
+
+//mongoose.connect('mongodb://localhost/test');
+
+//mongoose.connection.on('error', (err) => { 
+//    console.log('Mongodb Error: ', err); 
+//    process.exit();
+//});
+//mongoose.connection.on('connected', () => { 
+//    console.log('MongoDB is successfully connected');
+//});
+
+//app.listen(port, function(err){
+//    console.log('Listening on port: ' + port);
+//});
 
 app.listen(port, function(err){
     console.log('Listening on port: ' + port);
 });
+
+const dbURI = "mongodb://localhost/test";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
