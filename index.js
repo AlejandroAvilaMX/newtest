06c6fbd193bf91fb9e4527f5e8f996b18538ce20
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-01 00:02:36 +0000

    changes

--- index.js
+++ index.js
@@ -13,51 +13,43 @@ app.use(bodyParser.json());
 app.use(logger('tiny'));
 app.use(require('./routes'));
 
-//app.get('/hello/:foo/:bar', (req, res) => {
-//    //res.send('Hello World!');
-//    res.json({message: 'Hello World!', data: [
-//        req.params.foo,
-//        req.params.bar
-//    ]});
-//});
+// http.createServer((req, res)=>{
+//   res.write(users.join(", ")); //display the list of users on the page
+// //   res.write("\n\n"+emails.join(", ")); //display the list of users on the page
+//   res.end(); //end the response
+// }).listen(8000); // listen for requests on port 8000
 
-//app.post('/hello', (req, res) => {
-//    res.json({result: 'Post was sent', data: req.body});
-//});
+// let users = []; // names of users will be stored here
+// // let email = [];
+// (async function getNames(){
+//   try{
+//     const {data} = await axios.get("https://swapi.dev/api/people");
+//     console.log(data.results);
+//     users = data.results.map(user=>user.name);
+//     // emails = data.map(email=>email.email);
+//     console.log(users);
+//     // console.log(emails);
+//   } catch(error){
+//     console.log(error)
+//   }
+// })();
 
-//http.createServer((req, res)=>{
-//  //res.write("Hello world \n"); // write a response
-//  //res.write(users.join("\n"))
-//  res.write(chars.join("\n"))
-//  res.end(); //end the response
-//}).listen(8000); // listen for requests on port 8000
+// mongoose.connect('mongodb://localhost/test');
 
-//let users = []; // names of users will be stored here
-//let chars = [];
-//(async function getNames() {
-//  try {
-//    //const { data } = await axios.get(
-//    //  "https://jsonplaceholder.typicode.com/users"
-//    //);
-//    //users = data.map(user => user.name);
-//    const { data } = await axios.get("https://swapi.dev/api/people");
-//    chars = data.results.map(char => char.name)
-//    console.log(chars);
-//  } catch (error) {
-//    console.log(error);
-//  }
-//})();
-
-mongoose.connect('mongodb://localhost/test');
-
-mongoose.connection.on('error', (err) => { 
-    console.log('Mongodb Error: ', err); 
-    process.exit();
-});
-mongoose.connection.on('connected', () => { 
-    console.log('MongoDB is successfully connected');
-});
+// mongoose.connection.on('error', (err) => { 
+//     console.log('Mongodb Error: ', err); 
+//     process.exit();
+// });
+// mongoose.connection.on('connected', () => { 
+//     console.log('MongoDB is successfully connected');
+// });
 
 app.listen(port, function(err){
     console.log('Listening on port: ' + port);
 });
+
+const dbURI = "mongodb://localhost/test";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
