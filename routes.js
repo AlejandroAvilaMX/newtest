2240096d2c5435507b9c7fe4cfa5c55605975352
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-01 00:47:18 +0000

    new lines on routes.js

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require('express'),
 router = express.Router();
 
-
 var itemCtrl = require('./item-controller');
+userCtrl = require('./user-controller');
 
 router.get('/hello', itemCtrl.getWorld);
 
@@ -10,6 +10,8 @@ router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
 
 router.post('/hello', itemCtrl.postWorld);
 
+router.post('/users', userCtrl.createUser);
+router.get('/users', userCtrl.getUsers)
 //router.get('/hello/:foo/:bar', (req, res) => {
 //    //res.send('Hello World!');
 //    res.json({message: 'Hello World!', data: [
