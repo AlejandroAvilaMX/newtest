929d2a9229abf566cd1118fb21cd5f96ca0f837c
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-15 20:06:44 +0000

    multer

--- routes.js
+++ routes.js
@@ -5,29 +5,15 @@ var
 // itemCtrl = require('./item-controller'),
 userCtrl = require('./user-controller');
 
-router.get('/hello', itemCtrl.getWorld);
-
-router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
-
-router.post('/hello', itemCtrl.postWorld);
+// router.get('/hello', itemCtrl.getWorld);
+// router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+// router.post('/hello', itemCtrl.postWorld);
 
 router.post('/users', userCtrl.createUser);
 router.get('/users', userCtrl.getUsers);
 router.get('/users/:id', userCtrl.getUser);
 router.put('/users/:id', userCtrl.updateUser);
 router.delete('/users/:id', userCtrl.deleteUser);
-
-//router.get('/hello/:foo/:bar', (req, res) => {
-//    //res.send('Hello World!');
-//    res.json({message: 'Hello World!', data: [
-//        req.params.foo,
-//        req.params.bar
-//    ]});
-//});
-
-//router.post('/hello', (req, res) => {
-//    res.json({result: 'Post was sent', data: req.body});
-//});
 
 module.exports.UPLOAD_PATH = "uploads";
 
@@ -36,5 +22,8 @@ var upload = multer({ dest: module.exports.UPLOAD_PATH});
 var imageCtrl = require('./image-controller');
 
 router.post('/images', upload.single('image'), imageCtrl.uploadImage);
+router.get('/images', imageCtrl.getImages);
+router.get('/images/:id', imageCtrl.getImage);
+router.delete('/images/:id', imageCtrl.deleteImage);
 
 module.exports = router;
