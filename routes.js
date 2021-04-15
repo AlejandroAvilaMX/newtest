8b35f5777403b00ce2189bbe771a784cda1cac16
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-15 18:41:11 +0000

    new route

--- routes.js
+++ routes.js
@@ -33,4 +33,6 @@ var multer = require("multer");
 var upload = multer({ dest: module.exports.UPLOAD_PATH});
 var imageCtrl = require('./image-controller');
 
+router.post('images', upload.single('image'),imageCtrl.uploadImage);    
+
 module.exports = router;
