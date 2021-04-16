81c0c54a64d93d06a301ff9428dacdb8b81a7e16
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-16 00:07:39 +0000

    getImages

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http'),
-// axios = require('axios'),
+axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
@@ -52,4 +52,4 @@ const dbURI = "mongodb://localhost/test";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => console.log('connected to db'))
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err)); 
--- routes.js
+++ routes.js
@@ -5,9 +5,9 @@ var
 // itemCtrl = require('./item-controller'),
 userCtrl = require('./user-controller');
 
-// router.get('/hello', itemCtrl.getWorld);
-// router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
-// router.post('/hello', itemCtrl.postWorld);
+//router.get('/hello', itemCtrl.getWorld);
+//router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+//router.post('/hello', itemCtrl.postWorld);
 
 router.post('/users', userCtrl.createUser);
 router.get('/users', userCtrl.getUsers);
@@ -15,10 +15,24 @@ router.get('/users/:id', userCtrl.getUser);
 router.put('/users/:id', userCtrl.updateUser);
 router.delete('/users/:id', userCtrl.deleteUser);
 
+//router.get('/hello/:foo/:bar', (req, res) => {
+//    //res.send('Hello World!');
+//    res.json({message: 'Hello World!', data: [
+//        req.params.foo,
+//        req.params.bar
+//    ]});
+//});
+//router.post('/hello', (req, res) => {
+//    res.json({result: 'Post was sent', data: req.body});
+//});
+
 module.exports.UPLOAD_PATH = "uploads";
 
 var multer = require("multer");
 var upload = multer({ dest: module.exports.UPLOAD_PATH});
 var imageCtrl = require('./image-controller');
 
-module.exports = router; 
+router.post('/images', upload.single('image'), imageCtrl.uploadImage);
+router.get('/images', imageCtrl.getImages);
+
+module.exports = router;
